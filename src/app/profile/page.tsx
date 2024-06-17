fc295a595cff1c49dac88348b813c4676e57b380
hgiuda <hgiuda@gmail.com>
2024-06-17 14:22:38 +0000

    link

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,19 +1,41 @@
 'use client'
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 export default function Profile() {
     const router = useRouter()
     console.log(router);
-    
+
     return (
         <div>
             <h1>My list of profiles</h1>
             <div>
                 <ul>
-                    <li onClick={() => router.push('/profile/1')}>Profile 1</li>
-                    <li onClick={() => router.push('/profile/2')}>Profile 2</li>
-                    <li onClick={() => router.push('/profile/3')}>Profile 3</li>
-                    <li onClick={() => router.push('/profile/4')}>Profile 4</li>
-                    <li onClick={() => router.push('/profile/5')}>Profile 5</li>
+                    <li>
+                        <Link href="/profile/1">Profile 1</Link>
+                    </li>
+                    <li>
+                        <Link href="/profile/2">Profile 2</Link>
+                    </li>
+                    <li>
+                        <Link prefetch={false} scroll={false} href={{
+                            pathname: "/profile/3",
+                            query: {
+                                userName: "Hector",
+                                userId: 12,
+                                person: JSON.stringify({ height: 1.68 })
+                            }
+                        }}>
+                            <div>
+                                A big square where I got some text.
+                            </div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/profile/4">Profile 4</Link>
+                    </li>
+                    <li>
+                        <Link href="/profile/5">Profile 5</Link>
+                    </li>
                 </ul>
             </div>
         </div>
