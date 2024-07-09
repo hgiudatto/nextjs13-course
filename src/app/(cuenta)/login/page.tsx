d72aec84ee5c827cc5160e81dd8b395a28a4d641
hgiuda <hgiuda@gmail.com>
2024-07-09 19:50:54 +0000

    metadata and SEO

--- src/app/(cuenta)/login/page.tsx
+++ src/app/(cuenta)/login/page.tsx
@@ -1,3 +1,10 @@
+import { title } from "process"
+
+export const metadata = {
+    title: 'Login',
+    description: 'Login section'
+}
+
 export default function Login() {
     return <h1>Iniciar Sesión</h1>
 }
--- src/app/productos/[productId]/page.tsx
+++ src/app/productos/[productId]/page.tsx
@@ -1,9 +1,18 @@
-export default function PoductDetail(
-    { params }: {
-        params: {
-            productId: string
-        }
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string
     }
-) {
+}
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+    return {
+        title: `Producto ${params.productId}`
+    }
+}
+
+export default function PoductDetail(
+    { params }: Props) {
     return <h1>Producto {params.productId}</h1>
 }
